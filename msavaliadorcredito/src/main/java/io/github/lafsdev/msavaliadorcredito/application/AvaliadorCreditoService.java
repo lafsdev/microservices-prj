c5ee554d070dbd11c7705a7e81c89ceaeaa87cbb
lafsdev <lafs.dev@gmail.com>
2024-06-05 21:42:31 -0300

    Cartoes on cliente

--- msavaliadorcredito/src/main/java/io/github/lafsdev/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/github/lafsdev/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,23 +1,31 @@
 package io.github.lafsdev.msavaliadorcredito.application;
 
+import io.github.lafsdev.msavaliadorcredito.domain.model.CartaoCliente;
 import io.github.lafsdev.msavaliadorcredito.domain.model.DadosCliente;
 import io.github.lafsdev.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.github.lafsdev.msavaliadorcredito.infra.clients.CartoesResourcesClient;
 import io.github.lafsdev.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clienteResourceClient;
+    private final CartoesResourcesClient cartoesResourcesClient;
 
     public SituacaoCliente obterSituacaoCliente(String cpf) {
-        ResponseEntity<DadosCliente> responseEntity = clienteResourceClient.dadosCliente(cpf);
+        ResponseEntity<DadosCliente> responseEntityDadosCliente = clienteResourceClient.dadosCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> responseEntityCartoesClientes = cartoesResourcesClient.getCartoesClientes(cpf);
+
 
         return SituacaoCliente.builder()
-                .cliente(responseEntity.getBody())
+                .cliente(responseEntityDadosCliente.getBody())
+                .cartoes(responseEntityCartoesClientes.getBody())
                 .build();
     }
 }
